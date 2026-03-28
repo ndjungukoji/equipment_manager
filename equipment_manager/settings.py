518ad4e7f3e4d87afb69de68ac8c11c415ea929a
ndjungukoji <kojindjungu761@gmail.com>
2026-03-28 11:09:58 +0100

    Add files via upload

--- equipment_manager/settings.py
+++ equipment_manager/settings.py
@@ -117,4 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
